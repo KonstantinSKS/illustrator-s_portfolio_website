67d64c94a9c434c3403beee5d96699710b7d89ee
Константин Стеблев <konstantin.sks85@gmail.com>
2024-11-04 12:40:32 +0100

    f

--- victoria_site/admin_models.py
+++ victoria_site/admin_models.py
@@ -8,7 +8,6 @@
 #     db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
 # )
 
-
 # class Role(db.Model, RoleMixin):
 #     id = db.Column(db.Integer(), primary_key=True)
 #     name = db.Column(db.String(80), unique=True)
