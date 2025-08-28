ba44d8b3eadd34f010ce003a2e2c3973e52ad20e
Константин Стеблев <konstantin.sks85@gmail.com>
2025-08-28 19:17:51 +0200

    upd_network_config

--- victoria_site/routes.py
+++ victoria_site/routes.py
@@ -6,7 +6,7 @@ from .utils import make_cache_key
 
 
 @app.route('/')
-@cache.cached(timeout=60, key_prefix=make_cache_key)
+@cache.cached(timeout=300, key_prefix=make_cache_key)
 def index_view():
     """Renders main page with projects."""
     tag_filter = request.args.get('tag')
@@ -24,7 +24,7 @@ def index_view():
 
 
 @app.route('/projects/<int:id>')
-@cache.cached(timeout=60)
+@cache.cached(timeout=600)
 def project_view(id):
     """Renders project page."""
     project = Project.query.get_or_404(id)
@@ -33,14 +33,14 @@ def project_view(id):
 
 
 @app.route('/about')
-@cache.cached(timeout=60)
+@cache.cached(timeout=1800)
 def about_view():
     """Renders information about an artist."""
     return render_template('about.html', user=User.query.first())
 
 
 @app.route('/blogs')
-@cache.cached(timeout=60)
+@cache.cached(timeout=180)
 def all_blogs_view():
     """Renders main page with blogs."""
     blogs = Blog.query.all()
@@ -49,7 +49,7 @@ def all_blogs_view():
 
 
 @app.route('/blogs/<int:id>')
-@cache.cached(timeout=60)
+@cache.cached(timeout=600)
 def blog_view(id):
     """Renders blog page."""
     blog = Blog.query.get_or_404(id)
